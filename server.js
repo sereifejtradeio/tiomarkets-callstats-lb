04272a2b3169f8e17c4036e23711d6f1ecb6b885
Stavros Ereifej <stavros.ereifej@trade.io>
2020-08-06 09:36:06 +0300

    Call Stats Websocket

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const server = express()
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
-
+console.log(wss);
 wss.on('connection', (ws) => {
     console.log('Client connected');
     ws.on('close', () => console.log('Client disconnected'));
