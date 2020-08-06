bea0500a53589844ef26a0a13d904c6de70ddc3f
Stavros Ereifej <stavros.ereifej@trade.io>
2020-08-06 17:02:31 +0300

    Call Stats Websocket

--- includes/config/database.config.php
+++ includes/config/database.config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'eu-cdbr-west-03.cleardb.net');
+define('DB_HOST', 'us-mm-dca-3728a2b6d7d9.g5.cleardb.net');
 define('DB_USERNAME', 'b29a0a9071b63d');
 define('DB_PASSWORD', 'd420a826');
 define('DB_NAME', 'heroku_00410929162a0f1');
