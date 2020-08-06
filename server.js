0072b23e3ddaeb38c07e9d2e5f15314e0a02dfb6
Stavros Ereifej <stavros.ereifej@trade.io>
2020-08-06 10:21:26 +0300

    Call Stats Websocket

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
 
     console.log('Client connected');
+
     ws.on('close', () => console.log('Client disconnected'));
     
     setInterval(() => {
